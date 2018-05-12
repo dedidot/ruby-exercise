0ee3c3fa59e857ebdb1360dc8dfb38fd3cbd9881
ahmad sururi <ahmadsururi@ahmads-MacBook-Pro.local>
2018-05-12 16:11:58 +0700

    no 5

--- question-5.rb
+++ question-5.rb
@@ -1,18 +1,5 @@
 puts "Masukkan kalimat: "
-kalimat = gets.chomp
-
-#hilangkan spasi
-kalimat = kalimat.split("")
-
-counter = {}
-for i in kalimat
-	if i == " "
-		next 
-	elsif 
-		counter.has_key? i
-		counter[i] = counter[i] + 1
-	else
-		counter[i] = 1
-	end
-	puts "Jumlah karakter #{i} muncul sebanyak #{counter[i]}"
-end
+names = gets
+names = names.downcase.split("")
+data =  names.inject(Hash.new(0)) { |counts, name| counts[name] +=1; counts }
+data.each {|key, value| puts "#{key} muncul #{value} kali  " }
