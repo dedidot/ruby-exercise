6ea407c5f2e3149f0f5fe79a3ae70441d41dd641
ahmad sururi <ahmadsururi@ahmads-MacBook-Pro.local>
2018-05-12 15:03:24 +0700

    fix 6

--- question-6.rb
+++ question-6.rb
@@ -1,10 +1,25 @@
-#Masih OTW
-Class Calc
+class Calc
 	def aritmatika(data)
-		
+		data = data.split(" ")
+		operan = data[1]
+		bil_1 = data[0].to_i
+		bil_2 = data[2].to_i
+		if operan == '*'
+			bil_1 * bil_2
+		elsif operan == '-'
+			bil_1 - bil_2
+		elsif operan == '/'
+			bil_1 / bil_2
+		elsif operan == '+'
+			bil_1 + bil_2
+		else
+			"Gagal melakukan operasi"
+		end
 	end
 end
 
 puts "Masukkan calc: "
 data = gets
-puts data.split
+calcu = Calc.new
+calcu = calcu.aritmatika(data)
+puts calcu
