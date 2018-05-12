ac747655fa2ac3824323a6e498d2c672f1f4524f
ahmad sururi <ahmadsururi@ahmads-MacBook-Pro.local>
2018-05-12 21:42:56 +0700

    bug space count

--- question-5.rb
+++ question-5.rb
@@ -1,5 +1,5 @@
 puts "Masukkan kalimat: "
 names = gets
-names = names.downcase.split("")
+names = names.delete(" ").strip.downcase.split("")
 data =  names.inject(Hash.new(0)) { |counts, name| counts[name] +=1; counts }
 data.each {|key, value| puts "#{key} muncul #{value} kali  " }
